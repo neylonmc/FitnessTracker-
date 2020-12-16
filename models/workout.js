aa4f8e7024819c60328bb856317a7a1007d37a85
Meaghan Neylon <mneylon@richmond.edu>
2020-12-16 13:32:12 -0500

    Updated Schema for workouts.

---
+++ models/workout.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema; 
+
+const workoutSchema = new Schema({
+    date: {
+        type: DataCue,
+        default: Date.now
+    },
+    excerise: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Please enter an excerise type"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Please enter the excerise name."
+            },
+            weight: {
+                type: Number,
+                trim: true
+            },
+            sets: {
+                type: Number,
+                trim: true
+            },
+            reps: {
+                type: Number,
+                trim: true
+            },
+            duration: {
+                type: Number,
+                trim: true
+            }
+    }]
+}); 
+
+const Workouts = mongoose.model("Workouts", workoutSchema); 
+
+module.exports = Workouts;
