e48cd8d6723a0318cc928d88ae5ba7a0ca0084e4
Meaghan Neylon <mneylon@richmond.edu>
2020-12-19 13:05:31 -0500

    Fixed object to log exercise

--- models/workout.js
+++ models/workout.js
@@ -1,43 +1,47 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const Schema = mongoose.Schema; 
-
-const workoutSchema = new Schema({
-    date: {
+const WorkoutSchema = new Schema({
+    day: {
         type: Date,
         default: Date.now
     },
-    excerise: [
-            {
-            type: {
-                type: String,
-                trim: true,
-                required: "Please enter an excerise type"
-            },
-            name: {
-                type: String,
-                trim: true,
-                required: "Please enter the excerise name."
-            },
-            weight: {
-                type: Number,
-                trim: true
-            },
-            sets: {
-                type: Number,
-                trim: true
-            },
-            reps: {
-                type: Number,
-                trim: true
-            },
-            duration: {
-                type: Number,
-                trim: true
-            }
-    }]
-}); 
 
-const Workouts = mongoose.model("Workouts", workoutSchema); 
+  exercises:[{
 
-module.exports = Workouts;
+    type: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    name: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    weight: {
+        type: Number
+
+    },
+    reps: {
+        type: Number
+
+    },
+    sets: {
+        type: Number
+
+    },
+    distance: {
+        type: Number
+    }
+  }] 
+});
+
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
