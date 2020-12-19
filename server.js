336e2c132fd900c171577c0ed4d8befef167fe00
Meaghan Neylon <mneylon@richmond.edu>
2020-12-19 13:08:35 -0500

    Updated server

--- server.js
+++ server.js
@@ -3,23 +3,19 @@ const mongoose = require("mongoose");
 const logger = require("morgan");
 
 
-
-const PORT = process.env.PORT || 3000;
-
-const db = require("./models");
+const PORT = process.env.PORT || 8080;
 
 const app = express();
+app.use(logger("dev")); 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(logger("dev")); 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/workoutdb",
- {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
-  });
+});
 
 // routes
 app.use(require("./routes/apiRoutes.js"));
